3bd5838513a7a9849d896f83e3fbfd2d077c14eb
Srinivas Vaidyanathan <vaidyanathanrathka@gmail.com>
2020-05-24 17:57:44 +0530

    added initial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { HttpClientModule }    from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule
